b4a3b15ea41b173bb611d1e888cf486742dfa678
Joy Pratt <joympratt@gmail.com>
2014-09-23 13:10:01 -0500

    initial files

--- client/views/register/register.js
+++ client/views/register/register.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  angular.module('mean-template')
+  angular.module('casual-jams')
   .controller('RegisterCtrl', ['$scope', '$location', 'User', function($scope, $location, User){
     $scope.user = {};
 

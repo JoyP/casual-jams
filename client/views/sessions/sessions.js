9f5a2be0a5b2181aaf16520349d0a44900f87967
Joy Pratt <joympratt@gmail.com>
2014-10-05 20:59:16 -0500

    adds sections on sessions page

--- client/views/sessions/sessions.js
+++ client/views/sessions/sessions.js
@@ -4,10 +4,25 @@
   angular.module('casual-jams')
   .controller('SessionsCtrl', ['$scope', '$location', 'Sessions', function($scope, $location, Sessions){
     $scope.session = {};
+    $scope.mySessions = true;
 
     $scope.toggleNew = function(){
       console.log('$scope.newSession in session controller', $scope.newSession);
-      $scope.newSession = !!!$scope.newSession;
+      $scope.newSession = true;
+      $scope.findSession = false;
+      $scope.mySessions = false;
+    };
+
+    $scope.toggleFind = function(){
+      $scope.findSession = true;
+      $scope.newSession = false;
+      $scope.mySessions = false;
+    };
+
+    $scope.toggleMySessions = function(){
+      $scope.mySessions = true;
+      $scope.newSession = false;
+      $scope.findSession = false;
     };
 
 //    $scope.addNew() = function(){
